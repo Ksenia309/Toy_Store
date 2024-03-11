11fbd53379ddf181095fda9c4cfb0f02d1e76876
Ksenia309 <136187817+Ksenia309@users.noreply.github.com>
2024-03-12 00:40:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.PriorityQueue;
 
 public class ToyStore {
     public static void main(String[] args) {
